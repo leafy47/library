ffee4b4e9c59431f13186b3b9019ab0152f608a3
leafy47 <138401372+leafy47@users.noreply.github.com>
2023-10-23 23:44:47 +0100

    Most Javascript Complete

--- js.js
+++ js.js
@@ -13,16 +13,25 @@ const myLibrary = [
     }
 ];
 
-
-
-
-
+const radios = document.getElementsByName("read");
 const submit = document.querySelector('#submit');
 submit.addEventListener("click", clickAdd)
 let titlet = document.querySelector('#booktitle');
 let authora = document.querySelector('#author');
 let pagess = document.querySelector('#pages');
 let read = document.querySelector('input[name=read]');
+let form = document.querySelector('#form1');
+
+for(var i = 0; i < radios.length; i++){
+    radios[i].addEventListener('change', enableButton);
+}
+
+function enableButton() {
+    if(document.querySelector('input[name="read"]:checked')){
+      submit.disabled = false; 
+    }
+}
+
 
 function clickAdd(event) {
     if(!read) {
@@ -47,30 +56,6 @@ function Book(title, author, pageNum, read) {
     }
 }
 
-function display(myLibrary) {
-    for (i = 0; i < myLibrary.length; i++) {
-        let card = document.createElement("div");
-        card.classList.add('card');
-        let booktitle = document.createElement("p");
-        booktitle.textContent = `Title: ${myLibrary[i].title}`;
-        card.appendChild(booktitle);
-        let bookauthor = document.createElement("p");
-        bookauthor.textContent = `Author: ${myLibrary[i].author}`;
-        card.appendChild(bookauthor);
-        let page = document.createElement('p');
-        page.textContent = `Number of Pages: ${myLibrary[i].pageNum}`;
-        card.appendChild(page);
-        let read = document.createElement('p');
-        read.textContent = myLibrary[i].read ? `Read Yet?: Already Read` : `Read Yet?: Not Yet`;
-        card.appendChild(read);
-        let booktable = document.querySelector(".bookdisplay");
-        booktable.appendChild(card);
-        let del = document.createElement('button');
-        del.textContent = 'Delete';
-        card.appendChild(del);
-        del.addEventListener("click", delEntry);
-    }
-}
 
 function addBookToLibrary() {
     myLibrary.push({
@@ -92,24 +77,57 @@ function addBookToLibrary() {
     page.textContent = `Number of Pages: ${myLibrary[myLibrary.length - 1].pageNum}`;
     card.appendChild(page);
     let read = document.createElement('p');
-    read.textContent = myLibrary[i].read ? `Read Yet?: Already Read` : `Read Yet?: Not Yet`;
+    read.id = `read${myLibrary.length - 1}`;
+    let readx = document.querySelectorAll('input[name="read"]');
+    let value;
+for(let n = 0; n < readx.length; n++){
+  if(readx[n].checked){
+    value = readx[n].value;
+    break;
+  }
+}
+    if(value == 'false') {value = false} else {value = true};
+    read.textContent = value ? `Read Yet?: Already Read` : `Read Yet?: Not Yet`;
     card.appendChild(read);
     let booktable = document.querySelector(".bookdisplay");
     booktable.appendChild(card);
     let del = document.createElement('button');
-    del.id = `${myLibrary.length - 1}`;
+    del.classList.add(`${myLibrary.length - 1}`);
     del.textContent = 'Delete';
     card.appendChild(del);
     del.addEventListener("click", delEntry);
+    let rbutton = document.createElement('button');
+    rbutton.textContent = 'Toggle Read Status';
+    rbutton.classList.add(`${myLibrary.length - 1}`);
+    rbutton.addEventListener('click', toggleRead);
+    card.appendChild(rbutton);
+    reset();
 }
 
 function delEntry(event) {
-    delete myLibrary[this.id];
-    let delcard = document.querySelector(`#card${this.id}`);
+    delete myLibrary[this.className];
+    let delcard = document.querySelector(`#card${this.className}`);
     delcard.remove();
 }
 
-display(myLibrary);
+function toggleRead(event) {
+    let mantis = document.querySelector(`#read${this.className}`);
+    if(mantis.textContent == "Read Yet?: Already Read") {
+        mantis.textContent = "Read Yet?: Not Yet";
+        myLibrary[this.className].read = false;
+    }
+    else {
+        mantis.textContent = "Read Yet?: Already Read";
+        myLibrary[this.className].read = true;
+    }
+    
+}
+
+function reset() {
+    form.reset();
+    submit.disabled = true;
+}
+
 
 
 
