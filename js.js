4eeb5e3c4af16c514b4f7b85d39eed1243bf920c
leafy47 <138401372+leafy47@users.noreply.github.com>
2023-10-23 21:46:21 +0100

    Include Delete Function

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ const myLibrary = [
     {
         title: "Harry Potter and the Chamber of Secrets",
         author: "J.K. Rowling",
-        pagesNum: "251",
+        pageNum: "251",
         read: true
     },
     {
@@ -12,6 +12,30 @@ const myLibrary = [
         read: false
     }
 ];
+
+
+
+
+
+const submit = document.querySelector('#submit');
+submit.addEventListener("click", clickAdd)
+let titlet = document.querySelector('#booktitle');
+let authora = document.querySelector('#author');
+let pagess = document.querySelector('#pages');
+let read = document.querySelector('input[name=read]');
+
+function clickAdd(event) {
+    if(!read) {
+        return
+    }
+    if(titlet.value && authora.value && pagess.value) {
+        addBookToLibrary();
+    }
+    else {}
+    event.preventDefault();
+}
+
+
 
 function Book(title, author, pageNum, read) {
     this.title = title;
@@ -39,10 +63,50 @@ function display(myLibrary) {
         let read = document.createElement('p');
         read.textContent = myLibrary[i].read ? `Read Yet?: Already Read` : `Read Yet?: Not Yet`;
         card.appendChild(read);
-
         let booktable = document.querySelector(".bookdisplay");
         booktable.appendChild(card);
+        let del = document.createElement('button');
+        del.textContent = 'Delete';
+        card.appendChild(del);
+        del.addEventListener("click", delEntry);
     }
+}
+
+function addBookToLibrary() {
+    myLibrary.push({
+        title: titlet.value,
+        author: authora.value,
+        pageNum: pagess.value,
+        read: true
+    });
+    let card = document.createElement("div");
+    card.classList.add('card');
+    card.id = `card${myLibrary.length - 1}`;
+    let booktitle = document.createElement("p");
+    booktitle.textContent = `Title: ${myLibrary[myLibrary.length - 1].title}`;
+    card.appendChild(booktitle);
+    let bookauthor = document.createElement("p");
+    bookauthor.textContent = `Author: ${myLibrary[myLibrary.length - 1].author}`;
+    card.appendChild(bookauthor);
+    let page = document.createElement('p');
+    page.textContent = `Number of Pages: ${myLibrary[myLibrary.length - 1].pageNum}`;
+    card.appendChild(page);
+    let read = document.createElement('p');
+    read.textContent = myLibrary[i].read ? `Read Yet?: Already Read` : `Read Yet?: Not Yet`;
+    card.appendChild(read);
+    let booktable = document.querySelector(".bookdisplay");
+    booktable.appendChild(card);
+    let del = document.createElement('button');
+    del.id = `${myLibrary.length - 1}`;
+    del.textContent = 'Delete';
+    card.appendChild(del);
+    del.addEventListener("click", delEntry);
+}
+
+function delEntry(event) {
+    delete myLibrary[this.id];
+    let delcard = document.querySelector(`#card${this.id}`);
+    delcard.remove();
 }
 
 display(myLibrary);
